4649799b69693d498ff8192af416b256f9ee4411
Oosher <105375761+Oosher@users.noreply.github.com>
2023-07-26 00:12:23 +0300

    added preperation for a mui carucel

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -1,34 +1,33 @@
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import { Paper } from "@mui/material";
 
+export default function Page2(props) {
+    var items = [
+        {
+        name: "Item 1",
+        description: "Description 1",
+        },
+        {
+        name: "Item 2",
+        description: "Description 2",
+        },
+    ];
 
-
-import React from 'react'
-import Button from '@mui/material/Button'
-import { Box } from '@mui/material'
-
-export default function Page2() {
     return (
-        <Box sx={{display:"flex",flexDirection:"column"}}>
-        <Button variant="text" color="primary">
-        sad
-        </Button>
-        <Button variant="text" color="primary">
-        sad
-        </Button>
-        <Button variant="text" color="primary">
-        sad
-        </Button>
-        <Button variant="text" color="primary">
-        sad
-        </Button>
-        <Button variant="text" color="primary">
-        sad
-        </Button>
-        <Button variant="text" color="primary">
-        sad
-        </Button>
-        <Button variant="text" color="primary">
-        sad
-        </Button>
-        </Box>
-    )
+        <Carousel>
+        {items.map((item, i) => (
+            <Item key={i} item={item} />
+        ))}
+        </Carousel>
+    );
+    }
+
+    function Item(props) {
+    return (
+        <Paper>
+        <h2>{props.item.name}</h2>
+        <p>{props.item.description}</p>
+        </Paper>
+    );
 }
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,42 +1,57 @@
-
-
-
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { getProduct } from '../products/services/productServices';
-import { Box, Button, Typography } from '@mui/material';
+import React, {useEffect, useState} from 'react'
+import { useParams} from 'react-router-dom'
+import {getProduct} from '../products/services/productServices';
+import {Box,Button,CardMedia,Paper,Typography} from '@mui/material';
 
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Carousel from 'react-material-ui-carousel';
 
 export default function ProductPage() {
 
-    const [product,setProduct] = useState({})
+    const [product, setProduct] = useState({})
     const {id} = useParams();
 
-    
 
-    useEffect(()=>{
 
-        getProduct(id).then((result)=>setProduct(result));
+    useEffect(() => {
 
-    },[id])
+        getProduct(id).then((result) => setProduct(result));
+
+    }, [id])
 
     return (
-        <Box sx={{display:"flex",flexDirection:"column",width:"60%",margin:"0 auto"}}>
-
-            <Box sx={{display:"flex" , justifyContent:"start",gap:"5vw"}}>
-                <img src={product?.imageUrl} alt={product?.imageAlt} style={{height:"40vh",width:"30vw"}}/>
-                <Box sx={{marginTop:"5vh"}}>
-                    <Typography variant="h3" color="initial">{product?.name}</Typography>
-                    <Typography variant="body1" color="initial" sx={{marginTop:"2vh"}}>{product?.description}</Typography>
-
-
-                </Box>
-                
-            </Box>
-                    <Button variant="contained" color="info" sx={{width:"50%",alignSelf:"end"}}>
-                        <ShoppingCartIcon/>
-                    </Button>
+      <Box sx={{ display: "flex", flexDirection: "column", width: "60%", margin: "2vh auto " }}>
+        <Box sx={{ display: "flex", justifyContent: "start", gap: "2vw" }}>
+          <Carousel sx={{ width: "50%" }}>
+            <Item item={{ imageUrl: product?.imageUrl, imageAlt: product?.imageAlt }} />
+            <Item item={{ imageUrl: product?.imageUrl, imageAlt: product?.imageAlt }} />
+          </Carousel>
+          <Box sx={{ marginTop: "5vh" }}>
+            <Typography variant="h3" color="initial">
+              {product?.name}
+            </Typography>
+            <Typography variant="body1" color="initial" sx={{ marginTop: "2vh" }}>
+              {product?.description}
+            </Typography>
+            <Typography variant="body1" color="initial" sx={{marginTop:"219px"}}>&#8362;{product?.price}
+            </Typography>
+            <Button variant="contained" color="info" sx={{ width: "140%", alignSel: "flex-end" }}>
+              <ShoppingCartIcon />
+            </Button>
+          </Box>
         </Box>
-    )
+      </Box>
+    );
+}
+
+
+
+
+function Item (props){
+
+
+return(
+<Paper >
+    <CardMedia component="image" image={props.item.imageUrl} alt={props.item.imageAlt} sx={{ height: "40vh", width: "30vw" ,borderRadius:"1%" }} />
+</Paper>)
 }
