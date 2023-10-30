4f719535a9c9353013c03444ffa4fad6a7f0d103
Oosher <105375761+Oosher@users.noreply.github.com>
2023-10-30 23:43:19 +0200

    Update LikedProducts.jsx

--- src/pages/LikedProducts.jsx
+++ src/pages/LikedProducts.jsx
@@ -1,13 +1,17 @@
 
 
-import { Container, Grid } from '@mui/material'
+import { Container, Grid, Typography } from '@mui/material'
 import React from 'react'
 import { useProductService } from '../products/providers/ProductsProvider'
 import Product from '../products/product/Product';
 
 export default function LikedProducts() {
 
-    const {likedItems} = useProductService();
+    const {likedItems} = useProductService(); 
+
+    if(!likedItems)return <Container>
+      <Typography variant="h1" color="initial">You don't have any liked products yet</Typography>
+    </Container>
 
   return (
 <Container sx={{display:"flex",flexDirection:"column"}}>
