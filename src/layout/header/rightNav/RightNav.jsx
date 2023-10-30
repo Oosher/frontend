9af4234d773f572a84eab9cc9cc69fb37eb5e0a0
Oosher <105375761+Oosher@users.noreply.github.com>
2023-10-30 08:56:59 +0200

    Update RightNav.jsx

--- src/layout/header/rightNav/RightNav.jsx
+++ src/layout/header/rightNav/RightNav.jsx
@@ -7,7 +7,7 @@ import ShoppingCartButton from './ShoppingCartButton';
 
 export default function RightNav() {
     return (
-      <Box sx={{ display: "flex", gap: "1vw", alignItems: "center",width:"fit-content", marginRight: "5vw",justifyContent:"center"}}>
+      <Box sx={{ display: "flex", gap: "1vw", alignItems:"center",justifyContent:"center",justifyItems:"center",alignContent:"center",width:"fit-content", marginRight: "5vw"}}>
         <ShoppingCartButton />
         <Link to={ROUTS.LOGIN} style={{ textDecoration: "none" }}>
           <Typography variant="h6" color="initial">
