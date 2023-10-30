c0c2d5833042326e55aaa290c6c14d80e770550b
Oosher <105375761+Oosher@users.noreply.github.com>
2023-10-30 23:41:44 +0200

    Updates

--- src/layout/main/Content.jsx
+++
@@ -1,8 +0,0 @@
-import { Box } from '@mui/material'
-import React from 'react'
-
-export default function Content({children}) {
-    return (
-        <Box sx={{minHeight:"84.7vh"}}>{children}</Box>
-    )
-}
